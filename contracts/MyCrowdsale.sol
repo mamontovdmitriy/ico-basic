fdf8ced6a1d5d6c47a5df66b13ede8ab44f01a87
Dmitriy Mamontov <mamontovdmitriy@mail.ru>
2018-07-29 15:42:54 +0300

    comment for _rate

--- contracts/MyCrowdsale.sol
+++ contracts/MyCrowdsale.sol
@@ -2,10 +2,9 @@ pragma solidity ^0.4.24;
 
 import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 import 'zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
-//import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
 contract MyCrowdsale is MintedCrowdsale {
-    uint256 _rate = 1000;
+    uint256 _rate = 1000; // count of token per 1 ETH
 
     constructor(address _wallet, ERC20 _token) public Crowdsale(_rate, _wallet, _token) {}
 }
